e9629c01960d040dc9580dd47b51062763727d2e
Marina Sousa <151540994+marinasous4@users.noreply.github.com>
2024-06-17 22:21:38 -0300

    Index atualizado

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
     session_start();
-    require './verify/conexao.php';
+    require 'verify/conexao.php';
+    require './navbar.php';
         if(!isset($_SESSION['id'])){
         header('Location: login.php');
     }
+   
+$sql = 'SELECT * FROM filmes';
+$result = $conn->prepare($sql);
+$result->execute();
+$filmes = $result->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -11,15 +17,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Início</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h1>Seja bem-vindo!!</h1>
-                <a href="verify/logout.php" style="text-decoration: none; color: #845252;">Sair da conta</a>
-        </div>
-    </div>
+  <div class="card mb-3"  style="max-width: 18rem; background-color: #000000; color: #ffffff;">
+  <div class="card-body">
+    <h5 class="card-title">Filmes</h5>
+    <a class="btn" href="filmes.php" style="background-color: #981414; color: #ffffff;">Ver filmes</a>
+  </div>
+  </div>
+  <div class="card mb-3" style="max-width: 18rem; background-color: #000000; color: #ffffff;">
+  <div class="card-body">
+    <h5 class="card-title">Comidas</h5>
+    <a class="btn" href="" style="background-color: #981414; color: #ffffff;">Ver comidas</a>
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
