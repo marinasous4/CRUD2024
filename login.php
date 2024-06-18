24ff5e1748434bc88175998193bbc7e965f996c4
Marina Sousa <151540994+marinasous4@users.noreply.github.com>
2024-06-17 22:23:17 -0300

    Update login.php

--- login.php
+++ login.php
@@ -17,25 +17,25 @@ if (isset($_GET['success'])) {
 </head>
 <body>
   <form action="verify/logar.php" method="post" class="needs-validation" novalidate>
-    <section class="vh-100 gradient-custom">
+    <section class="vh-100" style="background-color: #981414;">
       <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
           <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div class="card text-white" style="border-radius: 3rem; background-color: #232323;">
+            <div class="card text-white" style="border-radius: 3rem; background-color: #000000;">
               <div class="card-body p-5 text-center">
                 <div class="mb-md-5 mt-md-4 pb-5">
                   <h2 class="fw-bold mb-2 text-uppercase p-3">Login</h2>
                   <div data-mdb-input-init class="form-outline form-white mb-4">
+                  <label class="form-label" for="typeEmailX">Email</label>
                     <input type="email" name="email" id="typeEmailX" class="form-control form-control-lg" required/>
-                    <label class="form-label" for="typeEmailX">Email</label>
                     <div class="invalid-feedback">Por favor, insira seu email.</div>
                   </div>
                   <div data-mdb-input-init class="form-outline form-white mb-4">
+                  <label class="form-label" for="typePasswordX">Senha</label>
                     <input type="password" name="senha" id="typePasswordX" class="form-control form-control-lg" required/>
-                    <label class="form-label" for="typePasswordX">Senha</label>
                     <div class="invalid-feedback">Por favor, insira sua senha.</div>
                   </div>
-                  <button data-mdb-button-init data-mdb-ripple-init class="btn btn-lg px-5 text-white fw-bold" style="background-color: #845252;" name="submit" type="submit" required>Entrar</button>
+                  <button data-mdb-button-init data-mdb-ripple-init class="btn btn-lg px-5 text-white fw-bold" style="background-color: #981414;" name="submit" type="submit" required>Entrar</button>
                   <div class="d-flex justify-content-center text-center mt-4 pt-1">
                   </div>
                 </div>
