f05dced707d5ca479d297eb47561a07725cfe36f
Marina Sousa <151540994+marinasous4@users.noreply.github.com>
2024-06-17 22:08:06 -0300

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class = 'navbar navbar-expand-lg navbar-light' style="background-color: #000000; color: #ffffff;">
+  <div class = 'container'>
+    <div class = 'offcanvas offcanvas-start-lg bg-danger' tabindex = '-1' id = 'offcanvasExample' aria-labelledby = 'offcanvasExampleLabel'>
+      <div class = 'offcanvas-body p-lg-0'>
+        <ul class = 'navbar-nav'>
+          <li class = 'nav-item'>
+            <a class = 'nav-link active' style="color: #ffffff;" aria-current = 'page' href = 'index.php'>Início</a>
+          </li>
+          <li class = 'nav-item'>
+            <a class = 'nav-link active' style="color: #ffffff;" aria-current = 'page' href = 'filmes.php'>Filmes</a>
+          </li>
+          <li class = 'nav-item'>
+            <a class = 'nav-link active' style="color: #ffffff;" aria-current = 'page' href = '#'>Comidas</a>
+          </li>
+          <li>
+          <a class="btn" href="login.php" style="background-color: #981414; color: #ffffff;">Sair da conta</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
